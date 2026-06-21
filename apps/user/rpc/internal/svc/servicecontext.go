eb116e1c0c898f40615ca7399feb4e0382297c93
fibneckt <1664991608@qq.com>
2026-06-21 16:39:06 +0800

    fix:user-rpc生成超级token让kafka服务器鉴权

--- apps/user/rpc/internal/svc/servicecontext.go
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -3,14 +3,18 @@ package svc
 import (
 	"IM/apps/user/models"
 	"IM/apps/user/rpc/internal/config"
+	"IM/pkg/constants"
+	"IM/pkg/ctxdata"
 	"context"
+	"time"
 
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config config.Config
-
+	*redis.Redis
 	models.UserModel
 }
 
@@ -26,8 +30,18 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	sqlConn := sqlx.NewMysql(c.Mysql.DataSource)
 
 	return &ServiceContext{
-		Config: c,
-
+		Config:    c,
+		Redis:     redis.MustNewRedis(c.Redisx),
 		UserModel: models.NewUserModel(sqlConn, c.Cache),
 	}
+}
+
+func (svc *ServiceContext) SetRootToken() error {
+	// 生成 jwt
+	systemToken, err := ctxdata.GetJwtToken(svc.Config.Jwt.AccessSecret, time.Now().Unix(), 9999999, constants.REDIS_SYSTEM_ROOT_TOKEN)
+	if err != nil {
+		return err
+	}
+	// 写入到redis
+	return svc.Redis.Set(constants.REDIS_SYSTEM_ROOT_TOKEN, systemToken)
 }
