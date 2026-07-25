03da6ca7311ecb6203d4b068be619e5560c26bfa
fibneckt <1664991608@qq.com>
2026-07-25 21:35:24 +0800

    chore:删除了一些烦人的todo

--- apps/user/api/internal/logic/user/detaillogic.go
+++ apps/user/api/internal/logic/user/detaillogic.go
@@ -31,8 +31,6 @@ func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogi
 }
 
 func (l *DetailLogic) Detail(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
-	// todo: add your logic here and delete this line
-
 	uid := ctxdata.GetUid(l.ctx)
 
 	userInfoResp, err := l.svcCtx.User.GetUserInfo(l.ctx, &user.GetUserInfoReq{
